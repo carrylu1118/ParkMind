fea25654167a9418757381ba7d58d34565a8bd05
Shawn <380014953@qq.com>
2025-09-22 14:04:11 +0800

    任务6

--- frontend/src/main/java/com/itheima/ai/tools/ClubTools.java
+++ frontend/src/main/java/com/itheima/ai/tools/ClubTools.java
@@ -15,28 +15,14 @@ public class ClubTools {
     @Autowired
     IClubReservationService clubReservationService;
 
-    @Tool(description = "生成社团预约单，返回预约单号")
+    @Tool(description = "xxxx") //TODO 任务6.2.2 工具描述，要与提示词一致才能触发
     public Integer createClubReservation(
-            @ToolParam(description = "预约社团") String club,
-            @ToolParam(description = "所在院系与专业") String major,
-            @ToolParam(description = "学生姓名") String name,
-            @ToolParam(description = "联系方式") String phone,
-            @ToolParam(description = "备注", required = false) String remark,
-            @ToolParam(description = "学号") String userId
+            @ToolParam(description = "学生姓名") String name
+            //更多需要采集的参数……与提示词相对应
             ) {
-        ClubReservation clubReservation = clubReservationService.getOne(new QueryWrapper<ClubReservation>().eq("user_id", userId));
-        if (clubReservation == null){
-            clubReservation = new ClubReservation();
-        }
-        clubReservation.setClub(club)
-                .setMajor(major)
-                .setName(name)
-                .setPhone(phone)
-                .setRemark(remark)
-                .setUserId(userId);
-
-        clubReservationService.saveOrUpdate(clubReservation);
-        log.info("学员预约社团登记：{}", clubReservation);
-        return clubReservation.getId();
+        //TODO 任务6.2.2 创建预约单，并返回预约单号
+        return 0;
     }
 }
+
+
